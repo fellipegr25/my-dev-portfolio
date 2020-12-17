ccba31a81e174947447b04f3afe347dafc655aee
Fellipe Geiger <fellipe.geiger@gmail.com>
2020-12-17 00:36:53 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
+import { useState } from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h4>Home 4</h4>
+      <Contador />
+      <div>Teste</div>
+    </div>
+  )
 }
 
+function Contador() {
+  const [contador,setContador] = useState(1);
+
+  function adicionarContador() {
+    setContador(contador + 1);
+  }
+
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={adicionarContador}>Adicionar</button>
+    </div>
+  )
+}
+ 
 export default HomePage
